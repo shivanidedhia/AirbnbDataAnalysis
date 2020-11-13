225305caeec3a7211379e003c7986d0ea5b89a2a
Shivani Dedhia <66826442+shivanidedhia@users.noreply.github.com>
2020-11-12 22:18:54 -0500

    updated file path

--- Initial Analysis.R
+++ Initial Analysis.R
@@ -6,6 +6,13 @@ library(tidyverse)
 
 ## Predicting new user booking. 
 
-imdb <- read.csv("/Users/shivani/Documents/Github/Airbnb/IMDM_ratings.csv")
+imdb <- read.csv("/Users/shivani/Documents/Github/UberDataAnalysis/IMDM_ratings.csv")
+
+str(imdb)
+
+# 5043 rows abd 28 cols
+dim(imdb)
 
 
+ggplot(aes(x = num_critic_for_reviews), data = movie) + geom_histogram(bins = 20, color = 'red') + ggtitle('Number of reviews')
+summary(imdb$num_critic_for_reviews)
