e8d8e6b1e3c415e2893f8254c2c1590640dace27
Nafis Ridwan Chowdhury <nafis121ridwan@gmail.com>
2020-12-09 14:54:05 -0600

    Update Initial Analysis.R

--- Initial Analysis.R
+++ Initial Analysis.R
@@ -173,17 +173,33 @@ rpart.plot(imdb_rpart,digits = 3)
 
 
 
+########################## Nafis #################################
+
+
+# Chart-1
+
+install.packages("plotly")
+
+library(plotly)
+
+
+imdb %>%
+  plot_ly(x = ~movie_facebook_likes, y = ~imdb_score, color = ~content_rating , mode = "markers", text = ~content_rating, alpha = 0.7, type = "scatter")
+
+
+# As expected, from the chart we can see that the higher the imdb rating
+# is, the higher the number of facebook like will be. However, there are some
+# outliers as well.
 
 
 
+# Chart-2
 
+ggplot(imdb, aes(x=imdb_score)) + geom_histogram(bins = 50)
 
-
-
-
-
-
-
+# Well, this simple bar chart shows that most of the movie ratings are between
+# 5 and 7.5. It means most of the movies had a good acceptance rate which is more
+# than 50%. 
 
 
 
